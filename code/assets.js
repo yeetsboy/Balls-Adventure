ba4a6493b885b6067bc21d473e70f0585c56dfa7
yeets_boy <117029080+yeetsboy@users.noreply.github.com>
2022-10-30 15:58:57 +0000

    Nothing

---
+++ code/assets.js
@@ -0,0 +1,16 @@
+export default function loadAssets() {
+	loadSprite("bean", "sprites/bean.png");
+	loadSprite("ghosty", "sprites/ghosty.png");
+	loadSprite("spike", "sprites/spike.png");
+	loadSprite("grass", "sprites/grass.png");
+	loadSprite("prize", "sprites/jumpy.png");
+	loadSprite("apple", "sprites/apple.png");
+	loadSprite("portal", "sprites/portal.png");
+	loadSprite("coin", "sprites/coin.png");
+  loadSprite("done", "sprites/you-are-awesome-well-done.gif");
+	loadSound("coin", "sounds/danger.mp3");
+	loadSound("powerup", "sounds/powerup.mp3");
+	loadSound("blip", "sounds/blip.mp3");
+	loadSound("hit", "sounds/error.mp3");
+	loadSound("portal", "sounds/portal.mp3");
+}
